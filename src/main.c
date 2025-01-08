c43eb8c107a60ae34fd430ffa24be533be0b5d34
toxypiks <HannaHinkelbein89@gmx.de>
2025-01-08 21:37:32 +0100

    Generate pixels and pipe them to ffmpeg for video generation

--- src/main.c
+++ src/main.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <stdint.h>
 #include <errno.h>
 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define READ_END 0
+#define WRITE_END 1
+
+#define WIDTH 800
+#define HEIGHT 600
+uint32_t pixels[WIDTH*HEIGHT];
+
 int main (void)
 {
+  int pipefd[2];
+
+  if (pipe(pipefd) < 0) {
+    fprintf(stderr, "ERROR: could not create pipe: %s\n", strerror(errno));
+    return 1;
+  }
+
   pid_t child = fork();
   if (child < 0) {
     fprintf(stderr, "ERROR: could not fork a child: %s\n", strerror(errno));
     return 1;
   }
   if (child == 0) {
+    // replace standard input of ffmpeg with the read end of the pipe (pipefd[0])
+    // dup2(pipefd[READ_END], STDIN_FILENO);
+    if (dup2(pipefd[READ_END], STDIN_FILENO) < 0) {
+      fprintf(stderr, "ERROR: could not reopen read end of pipe as stdin: %s\n", strerror(errno));
+      return 1;
+    }
+    close(pipefd[WRITE_END]);
     int ret = execlp("ffmpeg",
                      "ffmpeg",
-                     //"-i", "-",
-                     //"-f", "rawvideo",
+                     "-loglevel", "verbose",
+                     "-y",
+                     "-f", "rawvideo",
+                     "-pix_fmt", "rgba",
+                     "-s", "800x600",
+                     "-r", "60",
+                     "-an",
+                     "-i", "-",
+                     "-c:v", "libx264",
+
+                     "../output/output.mp4",
                      NULL
       );
     if (ret < 0) {
@@ -27,6 +58,17 @@ int main (void)
     }
     assert(0 && "unreachable");
   }
+  close(pipefd[READ_END]);
+
+  for (size_t i = 0; i < WIDTH*HEIGHT; ++i) {
+    pixels[i] = 0xFF0000FF;
+  }
+
+  for (size_t i = 0; i < 60; ++i) {
+    write(pipefd[WRITE_END], pixels, sizeof(*pixels)*WIDTH*HEIGHT);
+  }
+  close(pipefd[WRITE_END]);
+
   //waiting for child being finished with execution
   wait(NULL);
   printf("Done rendering the video\n");
