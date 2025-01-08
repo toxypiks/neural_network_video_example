02ad4ef8440490a2ff7368c254f4f1cc9a8251c2
toxypiks <HannaHinkelbein89@gmx.de>
2025-01-08 19:15:47 +0100

    Start simple child process from program

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <unistd.h>
 
 int main (void)
 {
-  printf("Hello World\n");
+  pid_t child = fork();
+  if (child < 0) {
+    fprintf(stderr, "ERROR: could not fork a child: %s\n", strerror(errno));
+    return 1;
+  }
+  if (child == 0) {
+    printf("Hello from child\n");
+    return 0;
+  }
+  printf("Hello from parent. The childs pid is %d\n", child
+    );
 
   return 0;
 }
