0e60286fec87bed9d9e69db940500783c4e85173
toxypiks <HannaHinkelbein89@gmx.de>
2025-01-18 14:14:07 +0100

    Create functional architecture for stack layout

--- src/layout.c
+++ src/layout.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 #include <raylib.h>
 #include <math.h>
 
@@ -26,9 +27,31 @@ typedef enum {
 typedef struct {
   Layout_Orient orient;
   Layout_Rect rect;
-  size_t i;
   size_t count;
+  size_t i;
 } Layout;
+
+Layout_Rect make_layout_rect(float x, float y, float w, float h)
+{
+  Layout_Rect r = {0};
+  r.x = x;
+  r.y = y;
+  r.w = w;
+  r.h = h;
+  return r;
+}
+
+// function to make sure all fields of Layout are initialized exept i, should be called everytime a new layout gets created
+// when adding new field to Layout this function will fail telling you to initialize field
+Layout make_layout(Layout_Orient orient, Layout_Rect rect, size_t count)
+{
+  Layout l = {0};
+  l.orient = orient;
+  l.rect = rect;
+  l.count = count;
+
+  return l;
+}
 
 Layout_Rect layout_slot(Layout *l)
 {
@@ -59,6 +82,43 @@ Layout_Rect layout_slot(Layout *l)
   return r;
 }
 
+//dynamic array
+typedef struct {
+  Layout *items;
+  size_t count;
+  size_t capacity;
+} Layout_Stack;
+
+#define DA_INIT_CAP 256
+#define da_append(da, item)                                                          \
+    do {                                                                             \
+        if ((da)->count >= (da)->capacity) {                                         \
+            (da)->capacity = (da)->capacity == 0 ? DA_INIT_CAP : (da)->capacity*2;   \
+            (da)->items = realloc((da)->items, (da)->capacity*sizeof(*(da)->items)); \
+            assert((da)->items != NULL && "Buy more RAM lol");                       \
+        }                                                                            \
+                                                                                     \
+        (da)->items[(da)->count++] = (item);                                         \
+    } while (0)
+
+void layout_stack_push(Layout_Stack *ls, Layout_Orient orient, Layout_Rect rect, size_t count)
+{
+  Layout l = make_layout(orient, rect, count);
+  da_append(ls, l);
+}
+
+Layout_Rect layout_stack_slot(Layout_Stack *ls)
+{
+  assert(ls->count > 0);
+  return layout_slot(&ls->items[ls->count-1]);
+}
+
+void layout_stack_pop(Layout_Stack *ls)
+{
+  assert(ls->count > 0);
+  ls->count -= 1;
+}
+
 int main(void)
 {
   size_t factor = 80;
@@ -69,30 +129,32 @@ int main(void)
   InitWindow(width, height, "test_layout");
   SetTargetFPS(60);
 
+  Layout_Stack ls = {0};
+
   while(!WindowShouldClose()) {
 
-    size_t padding = 100;
-    Layout root = {
-    .orient = LO_HORZ,
-    .rect = {0, padding, GetRenderWidth(), GetRenderHeight() - 2*padding},
-    .i = 0,
-    .count = 3,
-    };
+    float w = GetRenderWidth();
+    float h = GetRenderHeight();
+    size_t padding = h*0.15;
 
     BeginDrawing();
     ClearBackground(BLACK);
-    widget(layout_slot(&root), RED);
-    widget(layout_slot(&root), BLUE);
+
+    layout_stack_push(&ls, LO_HORZ, make_layout_rect(0, padding, w, h - 2*padding), 3);
+    widget(layout_stack_slot(&ls), RED);
+    widget(layout_stack_slot(&ls), BLUE);
+
     // nested widget
-    Layout panel = {
-      .orient = LO_VERT,
-      .rect = layout_slot(&root),
-      .count = 3,
-    };
-    widget(layout_slot(&panel), GREEN);
-    widget(layout_slot(&panel), YELLOW);
-    widget(layout_slot(&panel), PURPLE);
+    layout_stack_push(&ls, LO_VERT, layout_stack_slot(&ls), 3);
+    widget(layout_stack_slot(&ls), GREEN);
+    widget(layout_stack_slot(&ls), YELLOW);
+    widget(layout_stack_slot(&ls), PURPLE);
+    layout_stack_pop(&ls);
+    layout_stack_pop(&ls);
+
     EndDrawing();
+
+    assert(ls.count == 0);
   }
   return 0;
 }
